f1688c584c08f60888fe7ce99d51681b0e76c92d
Gönül Daşik <gonul3534@hotmail.com>
2022-10-28 19:20:34 +0300

    ikinci değişiklik

--- src/Github/github.java
+++ src/Github/github.java
@@ -3,7 +3,8 @@ package Github;
 public class github {
     public static void main(String[] args) {
         System.out.println("GitHub");
-
+        System.out.println("Kullanımı");
+        System.out.println("ile ilgili");
     }
 
 
